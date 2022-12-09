5c84128464f49353d98f6cab649b2a05246ee347
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-09 15:12:54 +0530

    complaint

---
+++ src/app/api.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+
+  adduser=(dataToSend:any)=>
+  {
+    return this.http.post("http://localhost:8080/ureg",dataToSend)
+  }
+
+  ulogin=(dataToSend:any)=>
+  {
+    return this.http.post("http://localhost:8080/userLogin",dataToSend)
+  }
+
+}
