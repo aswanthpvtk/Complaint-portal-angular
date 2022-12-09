de53056c2deb9554c8524d52bc1b006e4ba8d9db
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-09 15:03:36 +0530

    complaint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,13 @@ import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { FormsModule } from '@angular/forms';
 import { UserRegComponent } from './user-reg/user-reg.component';
 import { UserLoginComponent } from './user-login/user-login.component';
-
+import { RouterModule, Routes } from '@angular/router';
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AdminLoginComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +24,8 @@ import { UserLoginComponent } from './user-login/user-login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
