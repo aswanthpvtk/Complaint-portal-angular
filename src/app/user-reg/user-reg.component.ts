e2adc827423844a36b62969e5162a6a08d4252c9
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-09 14:54:42 +0530

    complaint

---
+++ src/app/user-reg/user-reg.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-reg',
+  templateUrl: './user-reg.component.html',
+  styleUrls: ['./user-reg.component.css']
+})
+export class UserRegComponent {
+
+
+  name=""
+  address=""
+  phone=""
+  email=""
+  uname=""
+  psw=""
+  cpsw=""
+
+
+// constructor(private api:ApiService){}
+  
+  readValues=()=>
+  {
+    let data:any={"name":this.name,"address":this.address,"phone":this.phone,"email":this.email,"uname":this.uname,"psw":this.psw,"cpsw":this.cpsw}
+    console.log(data)
+    // this.api.adduser(data).subscribe(
+    //   (response:any)=>
+    //   {
+    //     console.log(response)
+    //     if (response.status == "success") {
+
+    //       alert("Successfully added")
+  
+    //       this.name=""
+  
+    //       this.address=""
+
+    //       this.phone=""
+          
+    //       this.email=""
+  
+    //       this.uname=""
+    //       this.psw=""
+    //       this.cpsw=""
+  
+    //     } else {
+  
+    //       alert("Something went wrong")
+  
+    //     }
+    //   }
+    // )
+  }
+
+}
